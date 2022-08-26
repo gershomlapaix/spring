939a71d4330747922be29dde9f1153093aa91cbf
gershomlapaix <nsengiyumv33@gmail.com>
2022-08-26 17:31:31 +0200

    validation: make the integer input fields required

--- src/main/java/com/lapaix/springdemo/mvc/Customer.java
+++ src/main/java/com/lapaix/springdemo/mvc/Customer.java
@@ -13,9 +13,10 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 	
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be lest than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[ax-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
@@ -41,13 +42,13 @@ public class Customer {
 
 
 	
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
 
 	
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
