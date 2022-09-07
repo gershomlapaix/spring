2d5ca458b1de307dc2f07b90f0bb3f121885e55b
gershomlapaix <nsengiyumv33@gmail.com>
2022-09-07 21:30:17 +0200

    squeeze

--- spring-rest-demo/src/main/java/com/lapaix/spring_rest_demo/rest/StudentRestExceptionHandler.java
+++ spring-rest-demo/src/main/java/com/lapaix/spring_rest_demo/rest/StudentRestExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice // required first
 public class StudentRestExceptionHandler {
 
 	/** Adding an exception handler using @ExceptionHandler*/
