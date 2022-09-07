9691cf490e2fb1cb2a398542c909fdb5e9ea27cf
gershomlapaix <nsengiyumv33@gmail.com>
2022-09-07 20:11:39 +0200

    adding java annotations dependency

--- spring-rest-demo/src/main/java/com/lapaix/spring_rest_demo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/lapaix/spring_rest_demo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.lapaix.spring_rest_demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,11 +27,22 @@ public class StudentRestController {
 		theStudents.add(new Student("Nesta", "Mario"));
 	}
 	
+	
 	// define the endpoint for "/students"
 	
 	@GetMapping("/students")
-	public List<Student> getStudents(){	
+	public List<Student> getStudents(){
 		return theStudents;
+	}
+	
+	/**
+	 *  define the endpoint for "/students/{studentId}"
+	 *  @PathVarible binds the incoming request value with the identified parameter
+	 * */
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return theStudents.get(studentId);
 	}
 
 }
