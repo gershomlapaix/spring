3982d51de1d2ccad4e071996f4d572921cc20645
gershomlapaix <nsengiyumv33@gmail.com>
2022-09-07 11:15:14 +0200

    get the last naame and first name

--- jackson-demo/src/main/java/com/lapaix/jackson/json/demo/Driver.java
+++ jackson-demo/src/main/java/com/lapaix/jackson/json/demo/Driver.java
@@ -1,10 +1,28 @@
 package com.lapaix.jackson.json.demo;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			
+			// create the object mapper
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			// read JSON file and map/convert to JAVA POJO
+			Student theStudent = mapper.readValue(
+					new File("data/sample-lite.json"), Student.class);
+			
+			// print first name and last name
+			System.out.println("First name : "+ theStudent.getFirstName());
+			System.out.println("First name : "+ theStudent.getLastName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- jackson-demo/src/main/java/com/lapaix/jackson/json/demo/Student.java
+++ jackson-demo/src/main/java/com/lapaix/jackson/json/demo/Student.java
@@ -1,10 +1,39 @@
 package com.lapaix.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // please ignore any attribute not defined 
 public class Student {
 	
 	private int id;
 	private String firstName;
 	private String lastName;
 	private boolean active;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 
+	
 }
