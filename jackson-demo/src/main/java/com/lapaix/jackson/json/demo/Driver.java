0853544b5a19f7006c5bbc530e238820fdf473ed
gershomlapaix <nsengiyumv33@gmail.com>
2022-09-07 10:57:09 +0200

    student class

---
+++ jackson-demo/src/main/java/com/lapaix/jackson/json/demo/Driver.java
@@ -0,0 +1,10 @@
+package com.lapaix.jackson.json.demo;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jackson-demo/src/main/java/com/lapaix/jackson/json/demo/Student.java
@@ -0,0 +1,10 @@
+package com.lapaix.jackson.json.demo;
+
+public class Student {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private boolean active;
+
+}
